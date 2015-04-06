c18a5620ba8d1dd21dbb9af60ad15aa886d0a7d0
onstring <yanshi.403@gmail.com>
2015-04-07 00:14:53 +0800

    first web.py with template

---
+++ code.py
@@ -0,0 +1,21 @@
+import web
+
+urls = (
+    '/', 'index'
+)
+
+movies = [{'title': 'Forrest Gump',
+           'year': 1994,
+        },
+        {'title': 'Titanic',
+          'year':  1997,
+        }]
+render =  web.template.render('templates/')
+
+class index:
+    def GET(self):
+        return render.index(movies)
+
+if __name__ == "__main__":
+    app = web.application(urls, globals())
+    app.run()
